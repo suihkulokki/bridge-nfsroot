0f835a8da4ed19a7b4e7813b8cb6e28e5ab3dbf7
Riku Voipio <riku.voipio@linaro.org>
2016-05-25 14:37:05 +0300

    Add module support
    Signed-off-by: Riku Voipio <riku.voipio@linaro.org>

--- mkinitrd.sh
+++ mkinitrd.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 # script to generate initrd that sets up nfsroot on bridge
 
-rm -rf initramfs.cpio initramfs.cpio.gz tmp
-[ -r busybox-static_1.22.0-19_arm64.deb ] || wget http://ftp.fi.debian.org/debian/pool/main/b/busybox/busybox-static_1.22.0-19_arm64.deb
-dpkg-deb -x busybox-static*deb tmp/
+
+ARCH=$1
+URL=http://http.debian.net/debian/pool/main/b/busybox/busybox-static_1.22.0-9+deb8u1_${ARCH}.deb
+package=`basename $URL`
+wget -nv -nc $URL
+rm -rf initramfs.*.cpio initramfs.*.cpio.gz tmp
+dpkg-deb -x $package tmp/
 mkdir -p tmp/etc/udhcpc
 cp default.script tmp/etc/udhcpc
 rm -rf tmp/usr/
 cp init tmp/
-cd tmp; find . | cpio -H newc -o > ../initramfs.cpio
-cd ..; gzip initramfs.cpio 
+cd tmp; find . | cpio -H newc -o > ../initramfs.${ARCH}.cpio
+cd ..; gzip -9 initramfs.${ARCH}.cpio
 
