2b25f5e2c47f25418f3c5f0ac5852985fe274cd3
Riku Voipio <riku.voipio@linaro.org>
2017-05-10 12:58:51 +0300

    mkinitrd: make it possible to install modules
    If tmp/ exists, use it as base. This can be used to populate an
    initrd with modules from a kernel with:
    
    make install_modules INSTALL_MOD_PATH=/path/to/bridge-nfsroot/tmp/

--- mkinitrd.sh
+++ mkinitrd.sh
@@ -6,12 +6,12 @@ ARCH=$1
 URL=http://http.debian.net/debian/pool/main/b/busybox/busybox-static_1.22.0-9+deb8u1_${ARCH}.deb
 package=`basename $URL`
 wget -nv -nc $URL
-rm -rf initramfs.${ARCH}.cpio initramfs.${ARCH}.cpio.gz tmp
+rm -rf initramfs.${ARCH}.cpio initramfs.${ARCH}.cpio.gz
 dpkg-deb -x $package tmp/
 mkdir -p tmp/etc/udhcpc
 cp default.script tmp/etc/udhcpc
-rm -rf tmp/usr/
 cp init tmp/
 cd tmp; find . | cpio -H newc -o > ../initramfs.${ARCH}.cpio
 cd ..; gzip -9 initramfs.${ARCH}.cpio
+rm -rf tmp/
 
